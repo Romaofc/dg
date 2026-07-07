f3823a1779f305ccceba2d1cd440fd3224ea767a
Romaofc <douglascalves2009@gmail.com>
2026-07-07 12:17:31 -0300

    Atualizar o preparar-pc.ps1

--- preparar-pc.ps1
+++ preparar-pc.ps1
@@ -64,31 +64,3 @@ Write-Host ""
 Write-Host "           TOOLKIT v2.0" -ForegroundColor White
 Write-Host "==============================================" -ForegroundColor DarkCyan
 Write-Host ""
-
-Write-Host "1 - Teste"
-Write-Host "5 - Atalhos"
-
-$opcao = Read-Host "Escolha"
-
-if ($opcao -eq "1") {
-    Write-Host "Teste OK"
-}
-
-elseif ($opcao -eq "5") {
-
-    $DesktopPublico = "$env:PUBLIC\Desktop"
-
-    $WshShell = New-Object -ComObject WScript.Shell
-
-    $AtalhoASN = $WshShell.CreateShortcut("$DesktopPublico\Departamentos (ASN).lnk")
-    $AtalhoASN.TargetPath = "\\SVREP-ASN-001\Departamentos"
-    $AtalhoASN.Save()
-
-    $AtalhoTAG = $WshShell.CreateShortcut("$DesktopPublico\Departamentos (Tag).lnk")
-    $AtalhoTAG.TargetPath = "\\SVREP-TAG-001\Departamentos"
-    $AtalhoTAG.Save()
-
-    Write-Host "Concluido"
-}
-
-Pause
