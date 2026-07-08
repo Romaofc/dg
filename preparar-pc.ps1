2405b77dcf8aabaff4d8a321e487c08584765073
Romaofc <douglascalves2009@gmail.com>
2026-07-08 14:07:47 -0300

    Atualizar o preparar-pc.ps1

--- preparar-pc.ps1
+++ preparar-pc.ps1
@@ -1,7 +1,6 @@
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
 
     Start-Process powershell.exe -Verb RunAs -ArgumentList "-ExecutionPolicy Bypass -File `"$PSCommandPath`""
-    exit
 }
 
 Clear-Host
